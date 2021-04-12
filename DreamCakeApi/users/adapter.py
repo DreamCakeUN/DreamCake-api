0e1ced6f8f3fa970caf33a8b7d0349600b3023ef
z4yross <z4yross@gmail.com>
2021-04-11 21:08:08 -0500

    added rest auth

---
+++ DreamCakeApi/users/adapter.py
@@ -0,0 +1,6 @@
+from allauth.account.adapter import DefaultAccountAdapter
+
+
+class AccountAPIAdapter(DefaultAccountAdapter):
+    def respond_email_verification_sent(self, request, user):
+        pass
