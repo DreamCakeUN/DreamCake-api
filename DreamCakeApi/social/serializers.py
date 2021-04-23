ad569b3d52190383ff49547dd50e71a2054b22a5
cdsaineac <cdsaineac@unal.edu.co>
2021-04-22 23:25:52 -0500

    Arreglos en formatos del Serializer Posts

--- DreamCakeApi/social/serializers.py
+++ DreamCakeApi/social/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    usuario = serializers.StringRelatedField(many= False, read_only=True)
+    published_date = serializers.DateTimeField(format = '%Y-%h-%d ',read_only=True)
     class Meta:
-        model = Post
+        model = Post    
         fields = ['usuario','foto','likes','status','published_date']
         
