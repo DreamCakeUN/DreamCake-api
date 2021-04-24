d006cbc09500ae8b81fbab3a97689b89c779236d
Marcos <>
2021-04-23 22:04:02 -0500

    modificación media/banner

--- DreamCakeApi/banner/models.py
+++ DreamCakeApi/banner/models.py
@@ -6,7 +6,7 @@ class Banner(models.Model):
     text = models.TextField()
     abstract = models.CharField(max_length = 300)
     publication_date = models.DateTimeField(auto_now_add = True, null = True)
-    image = models.ImageField(null = True, blank  = True)
+    image = models.ImageField(upload_to='banner', null = True, blank  = True)
 
     def __str__(self):
         return self.title
